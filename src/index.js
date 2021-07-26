df80732e85c811f2d8beb8c48b708d3051d41f1a
Samantha Laine <samanthalaine8@gmail.com>
2021-07-26 12:26:12 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("#create-task-form")
+  form.addEventListener("submit", event => {
+    event.preventDefault()
+    createToDoItem(event.target.new_task_description.value)
+    form.reset()
+  })
 });
+
+function createToDoItem(item){
+  let toDo = document.createElement('li')
+  let taskBox = document.querySelector("#tasks")
+  let bttn = document.createElement('button')
+  bttn.addEventListener('click', handleDelete)
+  bttn.textContent = 'x'
+  toDo.textContent = `${item} `
+  toDo.appendChild(bttn)
+  taskBox.append(toDo)
+}
+
+function handleDelete(event){
+  event.target.parentNode.remove()
+}
